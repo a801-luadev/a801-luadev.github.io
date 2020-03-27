7e5aa32df1b06bbc91f7da967e1c0ded7d235ce9
JuniorGenius <drgenius@ca.rr.com>
2020-03-27 14:22:33 -0700

    Made data a 3D array, added more params

--- redirections.js
+++ redirections.js
@@ -1,11 +1,52 @@
+// Example:
+/*
 let data = {
-	"bolodefchoco": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=TSTEG3PXK4HJ4&source=url",
-	"signal": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=TSTEG3PXK4HJ4&source=url",
-	"cannonup": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=TSTEG3PXK4HJ4&source=url",
-	"infected": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=TSTEG3PXK4HJ4&source=url",
-	"crowdsurf": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=TSTEG3PXK4HJ4&source=url",
-	"hardcamp": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=TSTEG3PXK4HJ4&source=url",
-	"grounds": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=TSTEG3PXK4HJ4&source=url",
-	"bolodefchoco0perguntas": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=TSTEG3PXK4HJ4&source=url",
-	"powers": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=TSTEG3PXK4HJ4&source=url"
+    "bolodefchoco": { // Module name
+        "url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=TSTEG3PXK4HJ4&source=url", // URL to donation link
+        "icon": "kFrAYfd", // Imgur ID
+        "owner": "Bolodefchoco#0000", // Owner of the module and donation link
+        "host": "Bolodefchoco#0000" // Optional, only if different from owner
+    }
+}
+*/
+
+let data = {
+	"bolodefchoco": {
+		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=TSTEG3PXK4HJ4&source=url",
+		"owner": "Bolodefchoco#0000"
+	},
+	"signal": {
+		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=TSTEG3PXK4HJ4&source=url",
+		"owner": "Bolodefchoco#0000",
+		"host": "SomeoneElse#0000"
+	},
+	"cannonup": {
+		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=TSTEG3PXK4HJ4&source=url",
+		"icon": "",
+		"owner": "Bolodefchoco#0000"
+	},
+	"infected": {
+		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=TSTEG3PXK4HJ4&source=url",
+		"owner": "Bolodefchoco#0000"
+	},
+	"crowdsurf": {
+		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=TSTEG3PXK4HJ4&source=url",
+		"owner": "Bolodefchoco#0000"
+	},
+	"hardcamp": {
+		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=TSTEG3PXK4HJ4&source=url",
+		"owner": "Bolodefchoco#0000"
+	},
+	"grounds": {
+		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=TSTEG3PXK4HJ4&source=url",
+		"owner": "Bolodefchoco#0000"
+	},
+	"bolodefchoco0perguntas": {
+		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=TSTEG3PXK4HJ4&source=url",
+		"owner": "Bolodefchoco#0000"
+	},
+	"powers": {
+		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=TSTEG3PXK4HJ4&source=url",
+		"owner": "Bolodefchoco#0000"
+	}
 }
