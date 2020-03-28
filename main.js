7c47ce57fe490e8e33890eed562e8e106303e09f
Iván <tocutoeltuco@gmail.com>
2020-03-28 13:10:55 -0300

    Execute everything in window.onload

--- main.js
+++ main.js
@@ -8,27 +8,29 @@ function redirect() {
   window.location.href = module["url"].toLocaleLowerCase();
 }
 
-//Name
-document.getElementById("moduleName").textContent = "#" + moduleName.toString();
+window.onload = function() {
+  //Name
+  document.getElementById("moduleName").textContent = "#" + moduleName.toString();
 
-//Owner
-let owner = document.getElementById("moduleOwner");
-owner.textContent = "by " + module["owner"].toString();
+  //Owner
+  let owner = document.getElementById("moduleOwner");
+  owner.textContent = "by " + module["owner"].toString();
 
-/*
-//Host
-if("host" in module) {
-  owner.appendChild(document.createElement("br"));
-  owner.appendChild(document.createTextNode("(Hosted by " + module["host"].toString() + ")"));
-}
-*/
+  /*
+  //Host
+  if("host" in module) {
+    owner.appendChild(document.createElement("br"));
+    owner.appendChild(document.createTextNode("(Hosted by " + module["host"].toString() + ")"));
+  }
+  */
 
-//Icon
-if("icon" in module) {
-  document.getElementById("moduleIcon").src = "https://i.imgur.com/" + module["icon"] + ".png";
-}
+  //Icon
+  if("icon" in module) {
+    document.getElementById("moduleIcon").src = "https://i.imgur.com/" + module["icon"] + ".png";
+  }
 
-//Button
-if("url" in module) {
-  document.getElementById("donateButton").disabled = false;
+  //Button
+  if("url" in module) {
+    document.getElementById("donateButton").disabled = false;
+  }
 }
