b16248576dff35b55d6909f6695801a107931c33
Iván Gabriel <tocutoeltuco@gmail.com>
2020-03-31 17:05:10 -0300

    Restore stuff

---
+++ redirections.js
@@ -0,0 +1,72 @@
+// Example:
+/*
+data = {
+    "bolodefchoco": { // Module name
+        "url": "https://www.paypal.com/", // URL to donation link
+        "icon": "kFrAYfd", // Imgur ID
+        "owner": "Bolodefchoco#0000", // Owner of the module and donation link
+        "host": "Bolodefchoco#0000" // Optional, only if different from owner
+    }
+}
+*/
+
+data = {
+	"bolodefchoco": {
+		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=DLRQ67WGFYK7A&source=url",
+		"icon": "MIHrUyr",
+		"owner": "a"
+	},
+	"signal": {
+		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=DLRQ67WGFYK7A&source=url",
+		"icon": "28etCHe",
+		"owner": "Bolodefchoco#0000"
+	},
+	"cannonup": {
+		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=DLRQ67WGFYK7A&source=url",
+		"icon": "uhcnY3F",
+		"owner": "Bolodefchoco#0000"
+	},
+	"infected": {
+		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=DLRQ67WGFYK7A&source=url",
+		"icon": "RmWupLZ",
+		"owner": "Bolodefchoco#0000"
+	},
+	"crowdsurf": {
+		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=DLRQ67WGFYK7A&source=url",
+		"icon": "lyZO1rI",
+		"owner": "Bolodefchoco#0000"
+	},
+	"hardcamp": {
+		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=DLRQ67WGFYK7A&source=url",
+		"icon": "hnMrums",
+		"owner": "Bolodefchoco#0000"
+	},
+	"grounds": {
+		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=DLRQ67WGFYK7A&source=url",
+		"icon": "zBCSQ9i",
+		"owner": "Bolodefchoco#0000"
+	},
+	"bolodefchoco0perguntas": {
+		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=DLRQ67WGFYK7A&source=url",
+		"owner": "Bolodefchoco#0000"
+	},
+	"powers": {
+		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=DLRQ67WGFYK7A&source=url",
+		"icon": "GkMRm2r",
+		"owner": "Bolodefchoco#0000"
+	},
+	"parkour": {
+		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=VB6QN425S4SVU&item_name=Module+Team%27s+Donations+-+Parkour+Module&currency_code=USD&source=url",
+		"owner": "Tocutoeltuco#0000"
+	},
+	"circuit": {
+		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=YYQJZHPQH4FJ4&source=url",
+		"icon": "HGt6IRK",
+		"owner": "Ninguem#0095"
+	},
+	"unotfm": {
+		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=YYQJZHPQH4FJ4&source=url",
+		"icon": "cArlBKc",
+		"owner": "Ninguem#0095"
+	}
+}
