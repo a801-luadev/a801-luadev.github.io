f0a78fa03c3fad03b57439be32795b937bfa2ae2
Lautenschlager <tai.romani@hotmail.com>
2020-03-31 13:10:06 -0300

    update #bolodefchoco's URLs

--- redirections.js
+++ redirections.js
@@ -2,7 +2,7 @@
 /*
 data = {
     "bolodefchoco": { // Module name
-        "url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=TSTEG3PXK4HJ4&source=url", // URL to donation link
+        "url": "https://www.paypal.com/", // URL to donation link
         "icon": "kFrAYfd", // Imgur ID
         "owner": "Bolodefchoco#0000", // Owner of the module and donation link
         "host": "Bolodefchoco#0000" // Optional, only if different from owner
@@ -12,46 +12,46 @@ data = {
 
 data = {
 	"bolodefchoco": {
-		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=TSTEG3PXK4HJ4&source=url",
+		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=DLRQ67WGFYK7A&source=url",
 		"icon": "MIHrUyr",
 		"owner": "Bolodefchoco#0000"
 	},
 	"signal": {
-		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=TSTEG3PXK4HJ4&source=url",
+		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=DLRQ67WGFYK7A&source=url",
 		"icon": "28etCHe",
 		"owner": "Bolodefchoco#0000"
 	},
 	"cannonup": {
-		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=TSTEG3PXK4HJ4&source=url",
+		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=DLRQ67WGFYK7A&source=url",
 		"icon": "uhcnY3F",
 		"owner": "Bolodefchoco#0000"
 	},
 	"infected": {
-		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=TSTEG3PXK4HJ4&source=url",
+		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=DLRQ67WGFYK7A&source=url",
 		"icon": "RmWupLZ",
 		"owner": "Bolodefchoco#0000"
 	},
 	"crowdsurf": {
-		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=TSTEG3PXK4HJ4&source=url",
+		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=DLRQ67WGFYK7A&source=url",
 		"icon": "lyZO1rI",
 		"owner": "Bolodefchoco#0000"
 	},
 	"hardcamp": {
-		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=TSTEG3PXK4HJ4&source=url",
+		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=DLRQ67WGFYK7A&source=url",
 		"icon": "hnMrums",
 		"owner": "Bolodefchoco#0000"
 	},
 	"grounds": {
-		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=TSTEG3PXK4HJ4&source=url",
+		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=DLRQ67WGFYK7A&source=url",
 		"icon": "zBCSQ9i",
 		"owner": "Bolodefchoco#0000"
 	},
 	"bolodefchoco0perguntas": {
-		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=TSTEG3PXK4HJ4&source=url",
+		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=DLRQ67WGFYK7A&source=url",
 		"owner": "Bolodefchoco#0000"
 	},
 	"powers": {
-		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=TSTEG3PXK4HJ4&source=url",
+		"url": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=DLRQ67WGFYK7A&source=url",
 		"icon": "GkMRm2r",
 		"owner": "Bolodefchoco#0000"
 	}
