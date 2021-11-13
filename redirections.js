d18424f2cef3fced95ccb44147e95c936e76f9dd
Lautenschlager <tai.romani@hotmail.com>
2021-11-13 12:50:00 -0300

    #0000 -> #0015

--- redirections.js
+++ redirections.js
@@ -4,8 +4,8 @@ data = {
 	"bolodefchoco": { // Module name
 		"url": "https://www.paypal.com/", // URL to donation link
 		"icon": "kFrAYfd", // Imgur ID
-		"owner": "Bolodefchoco#0000", // Owner of the module and donation link
-		"host": "Bolodefchoco#0000" // Optional, only if different from owner
+		"owner": "Bolodefchoco#0015", // Owner of the module and donation link
+		"host": "Bolodefchoco#0015" // Optional, only if different from owner
 	}
 }
 */
@@ -14,46 +14,46 @@ data = {
 	"bolodefchoco": {
 		"url": "https://www.patreon.com/lautenschlager_id",
 		"icon": "MIHrUyr",
-		"owner": "Bolodefchoco#0000"
+		"owner": "Bolodefchoco#0015"
 	},
 	"signal": {
 		"url": "https://www.patreon.com/lautenschlager_id",
 		"icon": "28etCHe",
-		"owner": "Bolodefchoco#0000"
+		"owner": "Bolodefchoco#0015"
 	},
 	"cannonup": {
 		"url": "https://www.patreon.com/lautenschlager_id",
 		"icon": "uhcnY3F",
-		"owner": "Bolodefchoco#0000"
+		"owner": "Bolodefchoco#0015"
 	},
 	"infected": {
 		"url": "https://www.patreon.com/lautenschlager_id",
 		"icon": "RmWupLZ",
-		"owner": "Bolodefchoco#0000"
+		"owner": "Bolodefchoco#0015"
 	},
 	"crowdsurf": {
 		"url": "https://www.patreon.com/lautenschlager_id",
 		"icon": "lyZO1rI",
-		"owner": "Bolodefchoco#0000"
+		"owner": "Bolodefchoco#0015"
 	},
 	"hardcamp": {
 		"url": "https://www.patreon.com/lautenschlager_id",
 		"icon": "hnMrums",
-		"owner": "Bolodefchoco#0000"
+		"owner": "Bolodefchoco#0015"
 	},
 	"grounds": {
 		"url": "https://www.patreon.com/lautenschlager_id",
 		"icon": "zBCSQ9i",
-		"owner": "Bolodefchoco#0000"
+		"owner": "Bolodefchoco#0015"
 	},
 	"bolodefchoco0perguntas": {
 		"url": "https://www.patreon.com/lautenschlager_id",
-		"owner": "Bolodefchoco#0000"
+		"owner": "Bolodefchoco#0015"
 	},
 	"powers": {
 		"url": "https://www.patreon.com/lautenschlager_id",
 		"icon": "GkMRm2r",
-		"owner": "Bolodefchoco#0000"
+		"owner": "Bolodefchoco#0015"
 	},
 	"parkour": {
 		"url": "https://www.patreon.com/lautenschlager_id",//"https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=VB6QN425S4SVU&item_name=Module+Team%27s+Donations+-+Parkour+Module&currency_code=USD&source=url",
